244b1791ce0d83230bdab40d43ace8007e7c0c0c
Jamey Sharp <jamey@minilop.net>
2014-03-23 20:11:57 -0700

    Extend terms/rules to support value-passing LOTOS.

--- LOTOS.hs
+++ LOTOS.hs
@@ -1,28 +1,45 @@
 module LOTOS where
 
+import Control.Monad
 import Data.List
 import Data.Maybe
 
 type Gate = String
 
+type Name = String
+
+newtype Expression = Variable Name
+    deriving (Show, Eq)
+
+data ExitExpression = ExitExpression Expression | ExitAny
+    deriving Show
+
+data GateValue = ValueDeclaration Expression | VariableDeclaration Name
+    deriving Show
+
 data Behavior
     = Stop
-    | Action Gate Behavior
+    | Action Gate [GateValue] Behavior
     | Choice Behavior Behavior
     | Parallel [Gate] Behavior Behavior
     | Interleaving Behavior Behavior
     | Synchronization Behavior Behavior
     | Hide [Gate] Behavior
     | Process [Gate]
-    | Exit
-    | Sequence Behavior Behavior
+    | Exit [ExitExpression]
+    | Sequence Behavior [Name] Behavior
     | Preempt Behavior Behavior
-    deriving (Show, Eq)
+    deriving Show
+
+isTerminalBehavior :: Behavior -> Bool
+isTerminalBehavior (Exit _) = True
+isTerminalBehavior Stop = True
+isTerminalBehavior _ = False
 
 hideB :: [Gate] -> Behavior -> Behavior
-hideB gates (Action g b)
+hideB gates (Action g vs b)
     | g `elem` gates = hideB gates b
-    | otherwise = Action g $ hideB gates b
+    | otherwise = Action g vs $ hideB gates b
 hideB gates (Choice b1 b2) = Choice (hideB gates b1) (hideB gates b2)
 hideB gates b@(Parallel sync b1 b2)
     | not $ any (`elem` gates) sync = Parallel sync (hideB gates b1) (hideB gates b2)
@@ -30,21 +47,33 @@ hideB gates (Interleaving b1 b2) = Interleaving (hideB gates b1) (hideB gates b2
 hideB gates (Hide gates' b) = hideB (gates `union` gates') b
 hideB gates b@(Process gates') | not $ any (`elem` gates) gates' = b
 hideB _ Stop = Stop
-hideB _ Exit = Exit
-hideB gates (Sequence b1 b2) = Sequence (hideB gates b1) (hideB gates b2)
+hideB _ (Exit exprs) = Exit exprs
+hideB gates (Sequence b1 names b2) = Sequence (hideB gates b1) names (hideB gates b2)
 hideB gates (Preempt b1 b2) = Preempt (hideB gates b1) (hideB gates b2)
 hideB gates b = Hide gates b
 
-sequenceB :: Behavior -> Behavior -> Behavior
-sequenceB (Action g b1) b2 = Action g (sequenceB b1 b2)
-sequenceB Stop b = Stop
-sequenceB Exit b = b
-sequenceB a Exit = a
-sequenceB a b = Sequence a b
+sequenceB :: Behavior -> [Name] -> Behavior -> Behavior
+sequenceB (Action g vs b1) names b2 = Action g vs (sequenceB b1 names b2)
+sequenceB Stop _ _ = Stop
+sequenceB (Exit vs) names b | null [() | ExitAny <- vs] = sequenceRename vs names b
+sequenceB a [] (Exit []) = a
+sequenceB a names b = Sequence a names b
+
+sequenceRename :: [ExitExpression] -> [Name] -> Behavior -> Behavior
+sequenceRename exprs names | null binding = id
+    -- FIXME: actually renaming things is not yet implemented
+    where
+    binding = [(old, new) | (ExitExpression new, old) <- zip exprs names, Variable old /= new ]
 
 interleavingB :: Behavior -> Behavior -> Behavior
-interleavingB Exit b = b
-interleavingB a Exit = a
+interleavingB (Exit v1) (Exit v2) | length v1 == length v2 && all isJust merged = Exit $ map fromJust merged
+    where
+    merged = zipWith exitMerge v1 v2
+    exitMerge ExitAny b = Just b
+    exitMerge a ExitAny = Just a
+    exitMerge _ _ = Nothing
+interleavingB (Exit []) b = b
+interleavingB a (Exit []) = a
 interleavingB a b = Interleaving a b
 
 parallelB :: [Gate] -> Behavior -> Behavior -> Behavior
@@ -53,36 +82,57 @@ parallelB sync b1 b2 = maybe Stop flatten $ parallel' (`elem` sync) b1 b2
 synchronizationB :: Behavior -> Behavior -> Behavior
 synchronizationB b1 b2 = maybe Stop flatten $ parallel' (const True) b1 b2
 
-parallel' :: (Gate -> Bool) -> Behavior -> Behavior -> Maybe (Behavior, Behavior, Behavior)
-parallel' sync (Action g1 b1) b2
+parallel' :: (Gate -> Bool) -> Behavior -> Behavior -> Maybe (Behavior, Behavior, [Name], Behavior)
+parallel' sync (Action g1 v1 b1) b2
     | not (sync g1) = do
-        (l, r, b) <- parallel' sync b1 b2
-        return (Action g1 l, r, b)
-    | b2 == Exit || b2 == Stop = Nothing -- gate in sync can't match
-parallel' sync b1 (Action g2 b2)
+        (l, r, names, b) <- parallel' sync b1 b2
+        return (Action g1 v1 l, r, names, b)
+    | isTerminalBehavior b2 = Nothing -- gate in sync can't match
+parallel' sync b1 (Action g2 v2 b2)
     | not (sync g2) = do
-        (l, r, b) <- parallel' sync b1 b2
-        return (l, Action g2 r, b)
-    | b1 == Exit || b1 == Stop = Nothing -- gate in sync can't match
-parallel' sync (Action g1 b1) (Action g2 b2)
+        (l, r, names, b) <- parallel' sync b1 b2
+        return (l, Action g2 v2 r, names, b)
+    | isTerminalBehavior b1 = Nothing -- gate in sync can't match
+parallel' sync (Action g1 v1 b1) (Action g2 v2 b2)
     | g1 == g2 = do
-        rest <- parallel' sync b1 b2
-        return (Exit, Exit, Action g1 $ flatten rest)
+        guard $ length v1 == length v2 -- sync gates must have same attribute types
+        let freshNames = getFreshNames v1 v2
+        rest <- parallel' sync (rename freshNames v1 b1) (rename freshNames v2 b2)
+        let l = Exit $ map exitExpression v1
+        let r = Exit $ map exitExpression v2
+        return (l, r, freshNames, Action g1 (map (ValueDeclaration . Variable) freshNames) $ flatten rest)
     | otherwise = Nothing
 parallel' sync (Choice b1 b2) pb =
     case map flatten $ mapMaybe (parallel' sync pb) [b1, b2] of
     [] -> Nothing
-    (b:bs) -> Just (Exit, Exit, foldr Choice b bs)
+    (b:bs) -> Just (Exit [], Exit [], [], foldr Choice b bs)
 parallel' sync b1 b2@(Choice _ _) = parallel' sync b2 b1
-parallel' sync Exit Exit = Just (Exit, Exit, Exit)
+parallel' sync (Exit []) (Exit []) = Just (Exit [], Exit [], [], Exit [])
 parallel' _ a b = error $ "what to do with parallel' (" ++ show a ++ ") (" ++ show b ++ ")?"
 
-flatten :: (Behavior, Behavior, Behavior) -> Behavior
-flatten (l, r, b) = sequenceB (interleavingB l r) b
+exitExpression :: GateValue -> ExitExpression
+exitExpression (ValueDeclaration expr) = ExitExpression expr
+exitExpression (VariableDeclaration _) = ExitAny
+
+getFreshNames :: [GateValue] -> [GateValue] -> [Name]
+getFreshNames [] [] = []
+getFreshNames (VariableDeclaration name : v1) (_ : v2) = name : getFreshNames v1 v2
+getFreshNames (_ : v1) (VariableDeclaration name : v2) = name : getFreshNames v1 v2
+getFreshNames (ValueDeclaration (Variable name) : v1) (_ : v2) = name : getFreshNames v1 v2
+getFreshNames v1 v2 = error $ "getFreshNames: " ++ show v1 ++ " " ++ show v2
+
+rename :: [Name] -> [GateValue] -> Behavior -> Behavior
+rename freshNames decls | null binding = id
+    -- FIXME: actually renaming things is not yet implemented
+    where
+    binding = [(old, Variable new) | (VariableDeclaration old, new) <- zip decls freshNames, old /= new ]
+
+flatten :: (Behavior, Behavior, [Name], Behavior) -> Behavior
+flatten (l, r, names, b) = sequenceB (interleavingB l r) names b
 
 sample :: Behavior
 sample = hideB class_gates $ parallelB class_gates os_spec dev_spec
     where
     class_gates = ["class.send", "class.ok", "class.err"]
-    os_spec = (Action "os.req" (Action "class.send" (Choice (Action "class.ok" (Action "os.complete" Exit)) (Action "class.err" (Action "os.failed" Exit)))))
-    dev_spec = (Action "dev.enqueue" (Action "class.send" (Action "dev.irq" (Choice (Action "class.ok" Exit) (Action "class.err" Exit)))))
+    os_spec = (Action "os.req" [VariableDeclaration "msg"] (Action "class.send" [ValueDeclaration $ Variable "msg"] (Choice (Action "class.ok" [] (Action "os.complete" [] $ Exit [])) (Action "class.err" [VariableDeclaration "err"] (Action "os.failed" [ValueDeclaration $ Variable "err"] $ Exit [])))))
+    dev_spec = (Action "dev.enqueue" [VariableDeclaration "msg"] (Action "class.send" [ValueDeclaration $ Variable "msg"] (Action "dev.irq" [VariableDeclaration "err"] (Choice (Action "class.ok" [] $ Exit []) (Action "class.err" [ValueDeclaration $ Variable "err"] $ Exit [])))))
