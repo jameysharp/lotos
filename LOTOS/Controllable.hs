69bc1fc68c4b7bb4b2440a7b1cb48acea39968de
Jamey Sharp <jamey@minilop.net>
2014-03-28 16:45:07 -0700

    Replace deep Uniplate magic with a Writer monad.

--- LOTOS/Controllable.hs
+++ LOTOS/Controllable.hs
@@ -4,10 +4,12 @@ module LOTOS.Controllable (uncontrolled) where
 import LOTOS.AST
 
 import Control.Monad
-import Data.Generics.Str
+import Control.Monad.Trans.Class
+import Control.Monad.Trans.Writer
 import Data.Generics.Uniplate.Data
 import Data.List
 import qualified Data.Map as Map
+import Data.Monoid
 
 uncontrolled :: [Gate] -> Behavior -> Behavior
 uncontrolled gates (Interleaving b1 b2)
@@ -24,25 +26,30 @@ extractInterleavedAction b1 b2 = do
     Action g v b1' <- return b1
     common <- commonExit $ map (exitOf [name | VariableDeclaration name <- v]) $ exits b1'
     b2' <- if all (ExitAny ==) common then return b2 else do
-        (b2', free) <- para (unify common) b2
-        guard $ Map.null free
+        (b2', free) <- runWriterT $ unify common b2
+        guard $ all Map.null free
         return b2'
     return $ Action g v $ Interleaving b1' b2'
     where
-    unify common (Exit exprs) [] = Just (unboundExit, exitBinding) where
+    unify :: [ExitExpression] -> Behavior -> WriterT [Map.Map Variable Expression] Maybe Behavior
+    unify common (Exit exprs) = writer (unboundExit, [exitBinding]) where
         unboundExit = Exit [ case l of ExitAny -> r; _ -> ExitAny | (l, r) <- zip common exprs ]
         exitBinding = Map.fromList [ (var, expr) | (ExitExpression expr, ExitExpression (Variable var)) <- zip common exprs ]
-    unify common (Action g vs b) [Just (b', binding)] = Just (unboundAction, actionBinding) where
-        unboundAction = Action g (map updatingDecl vs) b'
-        updatingDecl (VariableDeclaration var) | Just expr <- Map.lookup var binding = ValueDeclaration expr
-        updatingDecl d = d
-        actionBinding = foldr Map.delete binding [ name | VariableDeclaration name <- vs ]
-    unify common b cs = do
-        (bs, bindings) <- liftM unzip $ sequence cs
-        guard $ same bindings
-        let (old, fromStr) = uniplate b
-        let (_, fromList) = strStructure old
-        return (fromStr $ fromList bs, head bindings)
+    unify common (Action g vs b) = do
+        (b', bindings) <- lift $ runWriterT $ unify common b
+        vs' <- case bindings of
+            [] -> return vs
+            [binding] -> do
+                let updatingDecl (VariableDeclaration var)
+                        | Just expr <- Map.lookup var binding = writer (ValueDeclaration expr, [var])
+                    updatingDecl d = return d
+                let (vs', shadowed) = runWriter $ mapM updatingDecl vs
+                writer (vs', [foldr Map.delete binding shadowed])
+        return $ Action g vs' b'
+    unify common b = do
+        (b', bindings) <- lift $ runWriterT $ descendM (unify common) b
+        guard $ null bindings || same bindings
+        writer (b', take 1 bindings)
 
 exits :: Behavior -> [[ExitExpression]]
 exits b = [exprs | Exit exprs <- universe b] -- FIXME: exclude Exits from Sequence LHS
