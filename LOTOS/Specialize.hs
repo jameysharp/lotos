d25414c9fc328700b43c2641d6d6cae927562591
Jamey Sharp <jamey@minilop.net>
2014-04-08 09:31:11 -0700

    Push Hide nodes through in specializeGates.
    It was not correct to specialize away gates that were introduced by a
    Hide node anyway, and this is the most convenient place to handle
    cross-process gate hiding.

--- LOTOS/Specialize.hs
+++ LOTOS/Specialize.hs
@@ -4,12 +4,14 @@ module LOTOS.Specialize (liftProcesses, specializeGates) where
 import LOTOS.AST
 import LOTOS.AST.Util
 import LOTOS.Util
+import LOTOS.Simplify
 
 import Control.Monad.Trans.Class
 import Control.Monad.Trans.Reader
 import Control.Monad.Trans.Writer
 import qualified Data.Map as Map
 import Data.Monoid
+import qualified Data.Set as Set
 import Unbound.LocallyNameless
 
 -- "Lambda-lifting" for processes. This is conceptually related to the
@@ -52,26 +54,31 @@ specializeGates p = runFreshM $ liftProcessesM p >>= transformProcess f
     where
     f formals procs b = do
         let scope = Map.fromList [ (name, p) | p@(Process name _) <- procs ]
-        (b', procs') <- flip runReaderT scope $ runMemoT $ specializeInstantiationGates b
+        (b', procs') <- flip runReaderT scope $ runMemoT $ specializeInstantiationGates Set.empty b
         return (formals, Map.elems procs', b')
 
-type SpecializeM a = MemoT (ReaderT (Map.Map (Name Process) Process) FreshM) (Name Process, [Gate]) Process a
+type SpecializeM a = MemoT (ReaderT (Map.Map (Name Process) Process) FreshM) (Name Process, [Gate], Set.Set Gate) Process a
 
-specializeInstantiationGates :: Behavior -> SpecializeM Behavior
-specializeInstantiationGates (Instantiate procname gates actuals) = do
-    Process procname' _ <- memoM specializeGatesWith (procname, gates)
+specializeInstantiationGates :: Set.Set Gate -> Behavior -> SpecializeM Behavior
+specializeInstantiationGates hidden (Instantiate procname gates actuals) = do
+    Process procname' _ <- memoM specializeGatesWith (procname, gates, hidden)
     return $ Instantiate procname' [] actuals
-specializeInstantiationGates b = descendBehavior specializeInstantiationGates b
+specializeInstantiationGates hidden (Hide binding) = do
+    (gs, b) <- unbind binding
+    b' <- specializeInstantiationGates (hidden `Set.union` Set.fromList gs) b
+    hideB gs b'
+specializeInstantiationGates hidden b = descendBehavior (specializeInstantiationGates hidden) b
 
-specializeGatesWith :: (Name Process, [Gate]) -> SpecializeM Process
-specializeGatesWith (procname, gates) = do
+specializeGatesWith :: (Name Process, [Gate], Set.Set Gate) -> SpecializeM Process
+specializeGatesWith (procname, gates, hidden) = do
     scope <- lift ask
     let Just process = Map.lookup procname scope
     if null gates then return process else do
     -- NOTE: liftProcesses ensured we have no nested processes
     Process _ body <- flip transformProcess process $ \ (formalGates, formalParams) [] b -> do
         let Just assign = mkPerm (map AnyName formalGates) (map AnyName gates)
-        b' <- specializeInstantiationGates $ swaps assign b
-        return (([], formalParams), [], b')
+        b' <- specializeInstantiationGates hidden $ swaps assign b
+        simplified <- hideB (Set.toList hidden) b'
+        return (([], formalParams), [], simplified)
     procname' <- fresh $ procname
     return $ Process procname' body
